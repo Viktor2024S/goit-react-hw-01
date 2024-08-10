ca8c6a9ac4e4ff47872155d82b07910a1227727d
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-08-10 01:42:00 +0100

    Created components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import react from "react";
 import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
 
-function App() {
-  return;
-  <></>;
+import Profile from "../Profile/Profile";
+import userData from "../../userData.json";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
 }
-
-export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,28 @@
-{
-  /* <div>
-  <div>
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-    />
-    <p>Petra Marica</p>
-    <p>@pmarica</p>
-    <p>Salvador, Brasil</p>
-  </div>
+import css from "./Profile.module.css";
 
-  <ul>
-    <li>
-      <span>Followers</span>
-      <span>1000</span>
-    </li>
-    <li>
-      <span>Views</span>
-      <span>2000</span>
-    </li>
-    <li>
-      <span>Likes</span>
-      <span>3000</span>
-    </li>
-  </ul>
-</div>; */
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={image} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
