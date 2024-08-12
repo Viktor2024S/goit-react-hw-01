229eb9d7dd354016a4674aeda4e39fc0faadf6f5
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-08-12 02:00:15 +0100

    repair task-2 and color of card task-1

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,7 +3,7 @@ import styles from "./FriendList.module.css";
 
 export default function FriendList({ friends }) {
   return (
-    <ul className={styles.FriendList}>
+    <ul className={styles.friendList}>
       {friends.map((friend) => (
         <li key={friend.id} className={styles.friendItem}>
           <FriendListItem
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,15 +1,12 @@
 import styles from "./FriendListItem.module.css";
+import clsx from "clsx";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div>
-      <img src={avatar} alt={name} width="48" className={styles.avatar} />
+    <div className={styles.friendList}>
+      <img src={avatar} alt={name} className={styles.avatar} />
       <p className={styles.name}>{name}</p>
-      <p
-        className={`${styles.status} ${
-          isOnline ? styles.online : styles.offline
-        }`}
-      >
+      <p className={clsx(isOnline ? styles.online : styles.offline)}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
