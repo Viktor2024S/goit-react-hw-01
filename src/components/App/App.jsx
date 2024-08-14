73e75c390de0bb18d6ab5d8b4666479353e2c947
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-08-14 14:16:00 +0100

    started task-3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,15 @@ import Profile from "../Profile/Profile";
 import userData from "../../userData.json";
 import FriendList from "../FriendList/FriendList";
 import friends from "../../friends.json";
+import TransactionHistory from "../FriendListItem/FriendListItem";
+import transactions from "../../transactions.json";
 
 export default function App() {
   return (
     <>
       <Profile {...userData} />
       <FriendList friends={friends} />
+      <TransactionHistory {...transactions} />
     </>
   );
 }
